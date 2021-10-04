16ea00d08b2ff691eaf6619272162f2fc666b88d
Jack <jacksonmorgan@hey.com>
2021-10-03 22:54:19 -0400

    start working on select elements for time and date

--- src/components/FetchUsers.jsx
+++ src/components/FetchUsers.jsx
@@ -13,50 +13,60 @@ class FetchUsers extends React.Component {
     return (
       <div className='form-container'>
         <textarea className='tweet-field'rows='6'></textarea>
-        <div className='month-day-year'>
-          <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-          </select>
-          <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-          </select>
-          <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-          </select>
-        </div>
         <div className='hour-min-sec'>
           <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
+            <option value='01'>01</option>
+            <option value='02'></option>
+            <option value='03'></option>
+            <option value='04'></option>
+            <option value='05'></option>
+            <option value='06'></option>
+            <option value='07'></option>
+            <option value='08'></option>
+            <option value='09'></option>
+            <option value='10'></option>
+            <option value='11'></option>
+            <option value='12'></option>
+            <option value=''></option>
+            <option value='05'></option>
           </select>
           <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
           </select>
           <select>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
-            <option></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+          </select>
+        </div>
+
+        <div className='month-day-year'>
+          <select>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+          </select>
+          <select>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+          </select>
+          <select>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
+            <option value=''></option>
           </select>
         </div>
 
